a148ca162dc7404f02fce8550ae50ccb877bb0a3
Colin Ogoo <ogoo.colin@gmail.com>
2025-06-28 01:28:00 +0000

    chore(shadcn components): install new shadcn components

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -27,7 +27,7 @@ function DialogOverlay({ className, ...props }: React.ComponentProps<typeof Dial
     <DialogPrimitive.Overlay
       data-slot="dialog-overlay"
       className={cn(
-        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-gray-900/20 backdrop-blur-sm',
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/20 backdrop-blur-sm',
         className,
       )}
       {...props}
